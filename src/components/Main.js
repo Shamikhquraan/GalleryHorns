d4a6aabcdd6250651e1b9ec39f0039f7b4ba4632
shamikhquraan1 <shamikhq@yahoo.com>
2021-08-09 02:43:14 +0300

    edditting

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,46 +3,52 @@ import React from 'react';
 import HornedBeast from './HornedBeast';
 
 class Main extends React.Component{
-
-
     render (){
 
-        let arrImg = [{
-            image_url: "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-            title: "UniWhal",
-            description: "A unicorn and a narwhal nuzzling their horns",
-            keyword: "narwhal",
-            horns: 1
-          },
-        
-          {
-            image_url: "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80",
-            title: "Rhino Family",
-            description: "Mother (or father) rhino with two babies",
-            keyword: "rhino",
-            horns: 2
-          },
-          
-          {
-            image_url: "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg",
-            title: "Unicorn Head",
-            description: "Someone wearing a creepy unicorn head mask",
-            keyword: "unicorn", 
-            horns: 1
-        }];
-        
+
+    let newArr = [{
+        image_url: "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
+        title: "UniWhal",
+        description: "A unicorn and a narwhal nuzzling their horns",
+        keyword: "narwhal",
+        horns: 1
+      },
     
-    return (
-    
+      {
+        image_url: "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80",
+        title: "Rhino Family",
+        description: "Mother (or father) rhino with two babies",
+        keyword: "rhino",
+        horns: 2
+      },
+      
+      {
+        image_url: "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg",
+        title: "Unicorn Head",
+        description: "Someone wearing a creepy unicorn head mask",
+        keyword: "unicorn", 
+        horns: 1
+    }];
+
+    return(
 <>
 
-<HornedBeast class='image' tittle='Horne I' imgeUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" description='great horney1' />
-<HornedBeast class='image' tittle='Horne II' imgeUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" description='great horney2' />
-<HornedBeast  class='image'tittle='Horne III' imgeUrl="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" description='great horney3' />
 
-    
-    </>
-)
+{
+newArr.map((item)=>{
+    return (
+
+
+<HornedBeast class='image' tittle={item.title} imgeUrl={item.image_url} description={item.description} />
+
+    )
+
+})
+   
+}
+</>
+    )
+
 }
 }
 
