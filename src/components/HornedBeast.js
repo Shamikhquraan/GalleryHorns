20ce9e5127806f29b61c75c2531fae7be1f96896
shamikhquraan1 <shamikhq@yahoo.com>
2021-08-09 20:37:22 +0300

    edditting

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+
 class HornedBeast extends React.Component{
 constructor(props){
 super(props);
@@ -15,7 +20,6 @@ this.setState({
     numberOfvote:this.state.numberOfvote+1
 })
 
-
 }
 
 
@@ -23,21 +27,31 @@ this.setState({
     
     return (
     
-<div >
-     <h2>{this.props.title} </h2>
-     <p>{this.props.horns}</p>
-     <img src={this.props.imgeUrl}  alt="hornedBeast" title="hornedBeast" />
-     <button onClick={this.addToVote} >Vote</button>
-     <p>Number of votes={this.state.numberOfvote}</p>
-     <p>{this.props.description}</p>
-     <p>{this.props.keyword}</p>
+     <>
 
-
-
-     </div>
+     <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={this.props.imgeUrl}/>
+  <Card.Body>
+    <Card.Title>{this.props.title} </Card.Title>
+    <Card.Text>
+      Vote through the button below :
+    </Card.Text>
+    <Button variant="primary" onClick={this.addToVote} > Go somewhere</Button>
+    <p>Number of votes={this.state.numberOfvote}</p>
+  </Card.Body>
+</Card>
+</>
    )
     
     }
 }
 
 export default HornedBeast;
+
+{/* <h2>{this.props.title} </h2>
+     <p>{this.props.horns}</p>
+     <img src={this.props.imgeUrl}  alt="hornedBeast" title="hornedBeast" />
+     <button onClick={this.addToVote} >Vote</button>
+     <p>Number of votes={this.state.numberOfvote}</p>
+     <p>{this.props.description}</p>
+     <p>{this.props.keyword}</p> */}
